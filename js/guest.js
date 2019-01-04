2dd7a77e2ec1fbd26d66816ffba7bdb5ff857342
aaronming <aaronming11@gmail.com>
2019-01-03 17:57:36 -0800

    - undo debug

--- js/guest.js
+++ js/guest.js
@@ -12,7 +12,7 @@ window.onload = function() {
     var StatsDataIndex = function(index) { return index * 5; }
     var SkillsDataIndex = function(index) { return index == 0 ? 0 : ((index - 1) * 20) + 8; }
 
-    var debug = 1;
+    var debug = 0;
 
     function MyApp() {
         var self = this;
